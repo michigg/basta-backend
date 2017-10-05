18043253bbf5f8ff6ffdd6869609037def4065fe
michigg <mgoetz1995@gmail.com>
2017-10-06 01:45:06 +0200

    wsgi config

--- ofu_app/ofu_app/settings.py
+++ ofu_app/ofu_app/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
     # },
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
