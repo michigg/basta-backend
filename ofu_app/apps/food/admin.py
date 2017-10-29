b23062965cc1581360da2ae2f1ea1cc4a6b08d05
michigg <michael-guenther.goetz@stud.uni-bamberg.de>
2017-10-30 00:23:32 +0100

    added user rating

--- ofu_app/apps/food/admin.py
+++ ofu_app/apps/food/admin.py
@@ -2,10 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from apps.food.models import SingleFood, Menu, HappyHour
+from apps.food.models import SingleFood, Menu, HappyHour, UserRating
 
 # Register your models here.
 admin.site.register(SingleFood)
 admin.site.register(Menu)
 admin.site.register(HappyHour)
+admin.site.register(UserRating)
 
