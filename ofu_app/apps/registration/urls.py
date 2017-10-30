3b08dd215f6423d5a74904def085614be0056cbb
michigg <michael-guenther.goetz@stud.uni-bamberg.de>
2017-10-31 00:42:18 +0100

    add user account view

--- ofu_app/apps/registration/urls.py
+++ ofu_app/apps/registration/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from apps.registration import views as core_views
 
 urlpatterns = [
-    url(r'^$', core_views.signup, name='signup'),
+    url(r'^$', core_views.account_view, name='account'),
+    url(r'^signup/$', core_views.signup, name='signup'),
     url(r'^account_activation_sent/$', core_views.account_activation_sent, name='account_activation_sent'),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         core_views.activate, name='activate'),
