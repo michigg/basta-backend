6b3fb138bc3a9304956793353cb3b219973c6f6c
clemens <git@clemensklug.de>
2018-01-15 11:25:55 +0100

    ...again

--- ofu_app/core/settings.py
+++ ofu_app/core/settings.py
@@ -160,9 +160,9 @@ TIME_FORMAT = "H:i"
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
 
 # Setup support for proxy headers
 USE_X_FORWARDED_HOST = True
