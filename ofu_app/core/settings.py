772a96f28f011971f4873e6f1a9940c67e458a2f
clemens <git@clemensklug.de>
2018-01-15 11:20:30 +0100

    set STATICROOT for production mode

--- ofu_app/core/settings.py
+++ ofu_app/core/settings.py
@@ -159,7 +159,7 @@ TIME_FORMAT = "H:i"
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
